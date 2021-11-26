b8ac71b7949176fd11ef4e460291720d15f2a434
TanYa-Go <tanja.godinic@gmail.com>
2021-11-26 18:33:49 +0000

    Add basic checkout functionality

--- babushka/settings.py
+++ babushka/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'checkout',
     
     # Other
-    'crispy-forms',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -176,8 +176,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#Stripe
 FREE_DELIVERY_THRESHOLD = 150
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 
 
 
