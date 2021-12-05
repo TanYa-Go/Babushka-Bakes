817cab0713b29a471d159361ae9c8f44babe22d9
TanYa-Go <tanja.godinic@gmail.com>
2021-12-05 18:10:30 +0000

    Remove secret key

--- babushka/settings.py
+++ babushka/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8t=lpwevl4o&b&ga2zjl=-ss-v*2f4h0)(e3e!#$0ye_r_70!3'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
