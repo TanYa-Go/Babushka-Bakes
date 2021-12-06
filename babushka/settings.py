d2d3a7b11dee01bc2b41e6c33356d8acf43c86e0
TanYa-Go <tanja.godinic@gmail.com>
2021-12-06 12:56:24 +0000

    Trying to fix deployment bug by reading logs

--- babushka/settings.py
+++ babushka/settings.py
@@ -10,16 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-
+from pathlib import Path
 import os
 import dj_database_url
+from os import path
+
 
 if os.path.exists("env.py"):
     import env
     DEBUG = True
 else:
     DEBUG = False
-    
+        
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
