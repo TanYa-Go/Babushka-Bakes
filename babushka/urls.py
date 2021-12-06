f2ff4043d1b26f093125e2caf2c0e7cc8d9a835f
TanYa-Go <tanja.godinic@gmail.com>
2021-12-06 21:53:27 +0000

    Remove unused images

--- babushka/urls.py
+++ babushka/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-     
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
