413b5bbf2f0dbea9ddd0061a618dac9502483d50
TanYa-Go <tanja.godinic@gmail.com>
2021-12-06 19:53:28 +0000

    Add email functionality

--- babushka/settings.py
+++ babushka/settings.py
@@ -235,7 +235,7 @@ DEFAULT_FROM_EMAIL = 'info@babushkabakes.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'info@babushkabakes.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
