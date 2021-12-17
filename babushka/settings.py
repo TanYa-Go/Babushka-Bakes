59e1cd4c0e373bae04193b2f00c754d914c0a0c0
TanYa-Go <tanja.godinic@gmail.com>
2021-12-17 22:17:11 +0000

    Implement sending emails from contact form

--- babushka/settings.py
+++ babushka/settings.py
@@ -225,7 +225,6 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'info@babushkabakes.com'
 
 
 if 'DEVELOPMENT' in os.environ:
--- contact/views.py
+++ contact/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
-from .forms import ContactForm
 from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+from .forms import ContactForm
+
 
 
 def contact(request):
     """ A view that renders the contact page"""
     submitted = False
     form = ContactForm
+    
+
     if request.method == 'POST':
         form = ContactForm(request.POST)
+        from_email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        
         if form.is_valid():
             form.save()
+            send_mail(subject, message, from_email, ['tanja.godinic@gmail.com'],)
             return HttpResponseRedirect('/contact?submitted=True')
-    else:    
+
+    else:
         form = ContactForm
         if 'submitted' in request.GET:
             submitted = True
