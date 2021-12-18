9ed4dbdad6854b88a7122a00609b67eebb5964f5
TanYa-Go <tanja.godinic@gmail.com>
2021-12-18 17:32:46 +0000

    Add html template to contact form email

--- contact/views.py
+++ contact/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from .forms import ContactForm
-
 
 
 def contact(request):
     """ A view that renders the contact page"""
     submitted = False
     form = ContactForm
-    
 
     if request.method == 'POST':
         form = ContactForm(request.POST)
         from_email = request.POST['email']
         subject = request.POST['subject']
         message = request.POST['message']
-        
+        html_email = render_to_string(
+                'emails/contact_form_template.html', {'from_email': from_email, 'subject': subject, 'message': message})
         if form.is_valid():
             form.save()
-            send_mail(subject, message, from_email, ['tanja.godinic@gmail.com'],)
+            send_mail(
+                subject, message, from_email, ['tanja.godinic@gmail.com'], html_message=html_email, fail_silently=False)
             return HttpResponseRedirect('/contact?submitted=True')
-
     else:
         form = ContactForm
         if 'submitted' in request.GET:
